5fa3cbc10525cec8c57684bc0b2662d968b64161
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-06-12 18:30:04 +0200

    there is no need for stun servers, either google already use default servers or it's not even needed, in both cases I don't have to specify it in my config

--- public/webrtc.js
+++ public/webrtc.js
@@ -1,18 +1,4 @@
-const stunServers = [
-  {
-    'urls': [
-      'stun:stun.l.google.com:19302',
-      'stun:global.stun.twilio.com:3478'
-    ]
-  }
-];
-
-const configuration = {
-  iceServers: stunServers,
-  sdpSemantics: 'unified-plan'
-};
-
-const peerConnection = new RTCPeerConnection(configuration);
+const peerConnection = new RTCPeerConnection();
 peerConnection.addEventListener('connectionstatechange', RTCConnectionChanged);
 function RTCConnectionChanged(event) {
   console.log("Connection Status changed", peerConnection.connectionState);
