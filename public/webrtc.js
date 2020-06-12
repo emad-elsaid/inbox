e447320f9b58a90d8cf6004002e0bdce8ec5c294
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-06-12 23:11:11 +0200

    reload the page if connection failed and remove page padding/margin

--- public/webrtc.js
+++ public/webrtc.js
@@ -1,5 +1,10 @@
 const peerConnection = new RTCPeerConnection();
 peerConnection.addEventListener('connectionstatechange', RTCConnectionChanged);
+
 function RTCConnectionChanged(event) {
   console.log("Connection Status changed", peerConnection.connectionState);
+
+  if( peerConnection.connectionState == 'failed' ) {
+    document.location.reload();
+  }
 }
